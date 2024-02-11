4d6f97e7840841290274ce6908da83b1584186d3
Het-Patel69 <phet9429@gmail.com>
2024-02-11 14:00:24 +0530

    styled testimonials slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,48 @@
-const navList = document.querySelector('nav');
-const navToggler = document.querySelector('.mobile-nav-toggler');
+const navList = document.querySelector("nav");
+const navToggler = document.querySelector(".mobile-nav-toggler");
 
-navToggler.addEventListener('click', () => {
-    const visibility = navList.getAttribute('data-visible')
-    if (visibility === "false") {
-        navList.setAttribute('data-visible', "true")
-        navToggler.setAttribute('area-expanded', "true")
-    }
-    else if (visibility === "true") {
-        navList.setAttribute('data-visible', "false")
-        navToggler.setAttribute('area-expanded', "false")
-    }
+navToggler.addEventListener("click", () => {
+  const visibility = navList.getAttribute("data-visible");
+  if (visibility === "false") {
+    navList.setAttribute("data-visible", "true");
+    navToggler.setAttribute("area-expanded", "true");
+  } else if (visibility === "true") {
+    navList.setAttribute("data-visible", "false");
+    navToggler.setAttribute("area-expanded", "false");
+  }
 });
 /*------------------------------- Slider--------------------------------*/
 
-
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
-const header = document.querySelector('header')
+const header = document.querySelector("header");
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY >= 600) {
-        header.classList.add('sticky')
-    } else {
-        header.classList.remove('sticky')
-    }
-})
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 600) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
 
 const swiper = new Swiper(".swiper", {
-    slidesPerView: 3.5,
+  slidesPerView: 3.5,
+  centeredSlides: true,
+  initialSlide: 2,
+  spaceBetween: 30,
 });
 
 const caseStudiesSwiper = new Swiper(".case-studies-swiper", {
-    slidesPerView: 1.2,
-    spaceBetween: 30,
-    //   centeredSlides: true,
+  slidesPerView: 1.2,
+  spaceBetween: 30,
+  centeredSlides: true,
 });
